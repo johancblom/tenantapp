5bcf831e3780d77c3542bd46fd8978a667080197
johancblom <johanblom@btinternet.com>
2016-11-06 18:36:06 +0000

    Updated tenant names and addresses

--- client/app/tenants/tenants.service.js
+++ client/app/tenants/tenants.service.js
@@ -4,8 +4,8 @@ angular.module('tenantappApp')
   .factory('Tenant', function () {
     var example_tenants =
       [
-        {_id: 1, title: 'Mr', initials: 'J.C.', surname: 'Blom', street: '6 Meadowview Road', town: 'London', plots: [{_id: 1, number: '7a', size: 2.5, in_use: true},{_id: 2, number: '7b', size: 5, in_use: true}], concess: 'N'},
-        {_id: 2, title: 'Mr', initials: 'J.C.', surname: 'Blom', street: '6 Meadowview Road', town: 'London', plots: [{_id: 3, number: '72a', size: 4, in_use: true},{_id: 4, number: '72b', size: 2.5, in_use: true}], concess: 'N'}
+        {_id: 1, title: 'Mr', initials: 'J.C.', surname: 'Dean', street: '6 Meadow Road', town: 'London', plots: [{_id: 1, number: '7a', size: 2.5, in_use: true},{_id: 2, number: '7b', size: 5, in_use: true}], concess: 'N'},
+        {_id: 2, title: 'Mr', initials: 'H.L.', surname: 'Baker', street: '6 Fleet Street', town: 'London', plots: [{_id: 3, number: '72a', size: 4, in_use: true},{_id: 4, number: '72b', size: 2.5, in_use: true}], concess: 'N'}
       ];
     return {
       query: function () {
